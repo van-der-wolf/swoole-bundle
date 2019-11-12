a963dc6af5504ff9ed2e197f50f44f45d3acb535
mfris <mfris@pixelfederation.com>
2019-11-12 21:38:08 +0100

    fatal error handling in HttpKernelRequestHandler

--- src/Bridge/Symfony/HttpKernel/HttpKernelRequestHandler.php
+++ src/Bridge/Symfony/HttpKernel/HttpKernelRequestHandler.php
@@ -8,22 +8,45 @@ use K911\Swoole\Bridge\Symfony\HttpFoundation\RequestFactoryInterface;
 use K911\Swoole\Bridge\Symfony\HttpFoundation\ResponseProcessorInterface;
 use K911\Swoole\Server\RequestHandler\RequestHandlerInterface;
 use K911\Swoole\Server\Runtime\BootableInterface;
+use Psr\Log\LoggerInterface;
 use Swoole\Http\Request as SwooleRequest;
 use Swoole\Http\Response as SwooleResponse;
 use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\HttpKernel\TerminableInterface;
+use Throwable;
 
 final class HttpKernelRequestHandler implements RequestHandlerInterface, BootableInterface
 {
+    /**
+     * @var KernelInterface
+     */
     private $kernel;
+
+    /**
+     * @var RequestFactoryInterface
+     */
     private $requestFactory;
+
+    /**
+     * @var ResponseProcessorInterface
+     */
     private $responseProcessor;
 
-    public function __construct(KernelInterface $kernel, RequestFactoryInterface $requestFactory, ResponseProcessorInterface $responseProcessor)
-    {
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    public function __construct(
+        KernelInterface $kernel,
+        RequestFactoryInterface $requestFactory,
+        ResponseProcessorInterface $responseProcessor,
+        LoggerInterface $logger
+    ) {
         $this->kernel = $kernel;
         $this->requestFactory = $requestFactory;
         $this->responseProcessor = $responseProcessor;
+        $this->logger = $logger;
     }
 
     /**
@@ -37,16 +60,42 @@ final class HttpKernelRequestHandler implements RequestHandlerInterface, Bootabl
     /**
      * {@inheritdoc}
      *
-     * @throws \Exception
+     * @param SwooleRequest  $request
+     * @param SwooleResponse $response
+     *
+     * @throws Throwable
      */
     public function handle(SwooleRequest $request, SwooleResponse $response): void
     {
-        $httpFoundationRequest = $this->requestFactory->make($request);
-        $httpFoundationResponse = $this->kernel->handle($httpFoundationRequest);
-        $this->responseProcessor->process($httpFoundationResponse, $response);
+        try {
+            $httpFoundationRequest = $this->requestFactory->make($request);
+            $httpFoundationResponse = $this->kernel->handle($httpFoundationRequest);
+            $this->responseProcessor->process($httpFoundationResponse, $response);
+        } catch (Throwable $e) {
+            $this->logger->critical(
+                'Unable to process request.',
+                [
+                    'error' => $e,
+                ]
+            );
 
-        if ($this->kernel instanceof TerminableInterface) {
-            $this->kernel->terminate($httpFoundationRequest, $httpFoundationResponse);
+            return;
+        }
+
+        try {
+            if ($this->kernel instanceof TerminableInterface) {
+                $this->kernel->terminate($httpFoundationRequest, $httpFoundationResponse);
+            }
+        } catch (Throwable $e) {
+            $this->logger->critical(
+                'Unable to terminate kernel. The Swoole worker will be reset.',
+                [
+                    'error' => $e,
+                ]
+            );
+
+            // it's probably best to rethrow the exception, so the swoole worker can be reset
+            throw $e;
         }
     }
 }
